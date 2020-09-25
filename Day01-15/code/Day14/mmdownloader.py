975e9cface4c8affbd281aaffa07d2eb6fbe16d7
greenfinchbaby@gmail.com <greenfinchbaby@gmail.com>
2020-09-25 10:02:22 +0800

    Day14 modified 天行api Url

---
+++ Day01-15/code/Day14/mmdownloader.py
@@ -0,0 +1,33 @@
+from time import time
+from threading import Thread
+
+import requests
+
+
+class DownloadHanlder(Thread):
+
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+
+    def run(self):
+        filename = self.url[self.url.rfind('\\') + 1:]
+        resp = requests.get(self.url)
+        with open('e:\\PythonDownloads\\' + filename, 'wb') as f:
+            f.write(resp.content)
+
+
+def main():
+    # 通过requests模块的get函数获取网络资源
+    resp = requests.get(
+        'http://api.tianapi.com/usermake/index?key=6f9dd432e7bf2ab15dde5f0b4dcdc9f9&num=10&urlid=133')
+    # 将服务器返回的JSON格式的数据解析为字典
+    data_model = resp.json()
+    for mm_dict in data_model['newslist']:
+        url = mm_dict['picUrl']
+        # 通过多线程的方式实现图片下载
+        DownloadHanlder(url).start()
+
+
+if __name__ == '__main__':
+    main()
